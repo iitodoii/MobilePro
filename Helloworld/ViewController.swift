51ef8d961e985948cde714b2776ed15569a9d65d
informaticsbuu <informatics@informaticsbuus-Mac-mini.local>
2018-02-02 16:50:51 +0700

    Mobile week 2

--- Helloworld/ViewController.swift
+++ Helloworld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showLabel: UILabel!
+        
+    @IBOutlet weak var showColor: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func showMessage(sender:UIButton){
+//        let alertController = UIAlertController(title: "Wellcome to My First APP",
+//                                                message: "Hello world" ,preferredStyle:UIAlertControllerStyle.actionSheet)
+//        alertController.addAction(UIAlertAction(title:"OK",style:UIAlertActionStyle.default, handler:nil))
+//        alertController.addAction(UIAlertAction(title:"Cancel",style:UIAlertActionStyle.destructive,handler:nil))
+//
+//        present(textest,animated:true,completion:nil)
+        
+        showLabel.text = "Hello world"
+        showColor.backgroundColor = UIColor.orange
+        showColor.setTitleColor(.white, for: .normal)
+        let selectButton = sender
+        
+        if let select = selectButton.titleLabel?.text{
+            showLabel.text = select
+        }
+        
+    }
 }
-
